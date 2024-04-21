cd96781d369d0eed3720b566aa2420425adc0531
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-04-21 14:40:42 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -73,8 +73,8 @@ resource "aws_instance" "example-01" {
 data "aws_instances" "existing_instance" {
   instance_tags = {
     Name = "your-instance-name"
-  instance_ids = var.instance_id
   }
+  instance_ids = [var.instance_id]
 }
 
 output "instance_id" {
